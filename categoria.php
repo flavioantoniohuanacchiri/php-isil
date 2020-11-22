26b7b94a0110aa69b123f4eadb6b6f5e8cb628c6
Edu Villegas <villegazed@gmail.com>
2020-11-22 18:16:59 -0500

    update categoria

--- categoria.php
+++ categoria.php
@@ -5,13 +5,13 @@
 			if (isset($_GET["action"])) {
 				switch ($_GET["action"]) {
 					case 'delete':
-						$usuarioJson = file_get_contents(__DIR__."/resources/assets/js/usuario.json");
-						$usuariosData = json_decode($usuarioJson, true);
+						$categoriaJson = file_get_contents(__DIR__."/resources/assets/js/categoria.json");
+						$categoriaData = json_decode($categoriaJson, true);
 						$tmpId = $_GET["id"];
-						$usuariosData[$tmpId]["deleted_at"] = date("Y-m-d H:i:s");
-						$usuarioJson = json_encode($usuariosData, JSON_UNESCAPED_UNICODE);
-						file_put_contents(__DIR__."/resources/assets/js/usuario.json", $usuarioJson);
-						$response = ["rst" => 1, "msj"=>"Usuario Eliminado"];
+						$categoriaData[$tmpId]["deleted_at"] = date("Y-m-d H:i:s");
+						$categoriaJson = json_encode($categoriaData, JSON_UNESCAPED_UNICODE);
+						file_put_contents(__DIR__."/resources/assets/js/categoria.json", $categoriaJson);
+						$response = ["rst" => 1, "msj"=>"Categoria Eliminada"];
 						echo json_encode($response);
 						exit;
 						break;
@@ -22,12 +22,12 @@
 				}
 			}
 			if (isset($_GET["id"])) {
-				$usuarioJson = file_get_contents(__DIR__."/resources/assets/js/usuario.json");
-				$usuariosData = json_decode($usuarioJson, true);
+				$categoriaJson = file_get_contents(__DIR__."/resources/assets/js/categoria.json");
+				$categoriaData = json_decode($categoriaJson, true);
 				$tmpId = $_GET["id"];
-				if (isset($usuariosData[$tmpId])) {
-					$usuariosData[$tmpId]["id"] = $tmpId;
-					echo json_encode($usuariosData[$tmpId]);
+				if (isset($categoriaData[$tmpId])) {
+					$categoriaData[$tmpId]["id"] = $tmpId;
+					echo json_encode($categoriaData[$tmpId]);
 					exit;
 				}
 
@@ -36,49 +36,39 @@
 	}
 	if (isset($_POST)) {
 		if (count($_POST) > 0) {
-			$usuarioJson = file_get_contents(__DIR__."/resources/assets/js/usuario.json");
-			$usuariosData = json_decode($usuarioJson, true);
+			$categoriaJson = file_get_contents(__DIR__."/resources/assets/js/categoria.json");
+			$categoriaData = json_decode($categoriaJson, true);
 			if (isset($_POST["id"]) && $_POST["id"] !="") {
 				$tmpId = $_POST["id"];
-				$tmpItem = $usuariosData[$tmpId];
+				$tmpItem = $categoriaData[$tmpId];
 				//print_r($tmpItem);
-				$usuariosData[$tmpId]["nombres"] = $_POST["nombres"];
-				$usuariosData[$tmpId]["ape_paterno"] = $_POST["ape_paterno"];
-				$usuariosData[$tmpId]["ape_materno"] = $_POST["ape_materno"];
-				$usuariosData[$tmpId]["sexo"] = $_POST["sexo"];
-				$usuariosData[$tmpId]["carrera"] = $_POST["carrera"];
-				$usuariosData[$tmpId]["grado"] = $_POST["grado"];
-				$usuariosData[$tmpId]["universidad"] = $_POST["universidad"];
-				$usuariosData[$tmpId]["anio_egreso"] = (int)$_POST["anio_egreso"];
+				$categoriaData[$tmpId]["codigo"] = $_POST["codigo"];
+				$categoriaData[$tmpId]["categoria"] = $_POST["categoria"];
+				$categoriaData[$tmpId]["descripcion"] = $_POST["descripcion"];
 
-				$usuariosData[$tmpId]["updated_at"] = date("Y-m-d H:i:s");
+				$categoriaData[$tmpId]["updated_at"] = date("Y-m-d H:i:s");
 				//print_r($tmpItem); exit;
-				$usuarioJson = json_encode($usuariosData, JSON_UNESCAPED_UNICODE);
-				file_put_contents(__DIR__."/resources/assets/js/usuario.json", $usuarioJson);
-				$response = ["rst" => 1, "msj"=>"Usuario Actualizado"];
+				$categoriaJson = json_encode($categoriaData, JSON_UNESCAPED_UNICODE);
+				file_put_contents(__DIR__."/resources/assets/js/categoria.json", $categoriaJson);
+				$response = ["rst" => 1, "msj"=>"Categoria Actualizada"];
 				echo json_encode($response);
 				exit;
 			} else {
 				$tmpItem = [];
-				$tmpItem["nombres"] = $_POST["nombres"];
-				$tmpItem["ape_paterno"] = $_POST["ape_paterno"];
-				$tmpItem["ape_materno"] = $_POST["ape_materno"];
-				$tmpItem["sexo"] = $_POST["sexo"];
-				$tmpItem["carrera"] = $_POST["carrera"];
-				$tmpItem["grado"] = $_POST["grado"];
-				$tmpItem["universidad"] = $_POST["universidad"];
-				$tmpItem["anio_egreso"] = (int)$_POST["anio_egreso"];
+				$tmpItem["codigo"] = $_POST["codigo"];
+				$tmpItem["categoria"] = $_POST["categoria"];
+				$tmpItem["descripcion"] = $_POST["descripcion"];
 				
 				$tmpItem["created_at"] = date("Y-m-d H:i:s");
 				$tmpItem["updated_at"] = "";
 				$tmpItem["deleted_at"] = "";
 
-				$size = count($usuariosData);
+				$size = count($categoriaData);
 				$size = $size +1;
-				$usuariosData[$size] = $tmpItem;
-				$usuarioJson = json_encode($usuariosData, JSON_UNESCAPED_UNICODE);
-				file_put_contents(__DIR__."/resources/assets/js/usuario.json", $usuarioJson);
-				$response = ["rst" => 1, "msj"=>"Usuario Creado"];
+				$categoriaData[$size] = $tmpItem;
+				$categoriaJson = json_encode($categoriaData, JSON_UNESCAPED_UNICODE);
+				file_put_contents(__DIR__."/resources/assets/js/categoria.json", $categoriaJson);
+				$response = ["rst" => 1, "msj"=>"Categoria Creada"];
 				echo json_encode($response);
 				exit;
 			}
@@ -94,7 +84,7 @@
 		<?php
 			include __DIR__."/resources/views/includes/head.phtml";
 		?>
-		<title>Usuarios</title>
+		<title>Categorias</title>
 	</head>
 	<body>
 		<?php
@@ -102,48 +92,46 @@
 		?>
 		<div class="container">
 			<?php 
-				//echo file_get_contents(__DIR__."/resources/assets/js/usuario.json"); exit;
-				$usuarioJson = file_get_contents(__DIR__."/resources/assets/js/usuario.json");
-				$usuariosData = json_decode($usuarioJson, true);
+				//echo file_get_contents(__DIR__."/resources/assets/js/categoria.json"); exit;
+				$categoriaJson = file_get_contents(__DIR__."/resources/assets/js/categoria.json");
+				$categoriaData = json_decode($categoriaJson, true);
 			?>
 			<div class="box box-primary content-table">
-				<h1>Listado de Usuarios <div style="width: auto; display: inline-block; float: right;">
+				<h1>Listado de Categoria <div style="width: auto; display: inline-block; float: right;">
 					<a href="#" class="btn btn-primary"
 						data-toggle="modal"
-						data-target="#mdlUsuario">
+						data-target="#mdlCategoria">
 						<i class="fas fa-plus"></i> Agregar</a>
 					</div></h1>
 			<div class="table-responsive-md">
-  				<table id="table-usuarios" class="table table-striped table-bordered nowrap" style="width:100%">
+  				<table id="table-categoria" class="table table-striped table-bordered nowrap" style="width:100%">
 						<thead>
 						    <tr>
 						      <th>#</th>
-						      <th>Nombres</th>
-						      <th>Ape Paterno</th>
-						      <th>Ape Materno</th>
-						      <th>Sexo</th>
+						      <th>Codigo</th>
+						      <th>Categoria</th>
+						      <th>Descripcion</th>
 						      <th>U.Act.</th>
 						      <th>[]</th>
 						    </tr>
 						</thead>
 						<tbody>
 						  		<?php 
-						  			//print_r($usuariosData); exit;
-						    		foreach ($usuariosData as $key => $value) {
+						  			//print_r($categoriaData); exit;
+						    		foreach ($categoriaData as $key => $value) {
 						    			$tmpIndex = (int)$key;
 						    			if ($value["deleted_at"] == "") {
 						    	?>
 							    <tr>
 							      <th><?php echo $tmpIndex;?></th>
-							      <td><?php echo $value["nombres"];?></td>
-							      <td><?php echo $value["ape_paterno"];?></td>
-							      <td><?php echo $value["ape_materno"];?></td>
-							      <td><?php echo $value["sexo"];?></td>
+							      <td><?php echo $value["codigo"];?></td>
+							      <td><?php echo $value["categoria"];?></td>
+							      <td><?php echo $value["descripcion"];?></td>
 							      <td><?php echo $value["updated_at"];?></td>
 							      <td>
 							      	<a href="#"
 							      		data-toggle="modal"
-							      		data-target="#mdlUsuario"
+							      		data-target="#mdlCategoria"
 							      		class="btn btn-primary"
 							      		data-id="<?php echo $key;?>">
 							      		<i class="fas fa-pencil-alt"></i>
@@ -162,14 +150,14 @@
 			
 		</div>
 		<?php 
-			include __DIR__."/resources/views/modal/mdl_usuario.phtml";
+			include __DIR__."/resources/views/modal/mdl_categoria.phtml";
 		?>
 		<?php
 			include __DIR__."/resources/views/includes/script.phtml";
 			include __DIR__."/resources/views/includes/loading.phtml";
 		?>
 		<script type="text/javascript">
-			var table = $('#table-usuarios').DataTable({
+			var table = $('#table-categoria').DataTable({
 			   	"language": {
 			        "url": "/Spanish.json"
 			    },
@@ -181,6 +169,6 @@
 			    //new $.fn.dataTable.FixedHeader( table );
 			} );
 		</script>
-		<script type="text/javascript" src="js/web/usuario.js"></script>
+		<script type="text/javascript" src="js/web/categoria.js"></script>
 	</body>
 </html>

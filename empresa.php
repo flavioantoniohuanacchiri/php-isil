64f235978ba2a1c4841507120d162e0cdc797c7b
Julio Alberto Moises Calderon Gonzales <jcalderon@isil.isil.edu.pe>
2020-11-23 18:45:01 -0500

    cambios nuevos

--- empresa.php
+++ empresa.php
@@ -22,12 +22,18 @@
 			include __DIR__."/resources/views/includes/header.phtml";
 		?>
 		<div class="container">
+		<?php 
+				//echo file_get_contents(__DIR__."/resources/assets/js/usuario.json"); exit;
+				$usuarioJson = file_get_contents(__DIR__."/resources/assets/js/empresa.json");
+				$usuariosData = json_decode($usuarioJson, true);
+			?>
 			<div class="box box-primary content-table">
-				<h1>Listado de Usuarios</h1>
+				<h1>Listado de Empresas</h1>
 				<div class="table-responsive-md">
 	  				<table id="table-empresa" class="table table-striped table-bordered nowrap" style="width:100%">
 						<thead>
 				            <tr>
+								<th>#</th>
 				                <th>Name</th>
 				                <th>Position</th>
 				                <th>Office</th>
@@ -43,18 +49,19 @@
 						    		foreach ($usuariosData as $key => $value) {
 						    	?>
 							    <tr>
-							      <td><?php echo $value["name"];?></td>
+								  <th><?php echo $key;?></th>
+							      <td><?php echo $value["first_name"];?></td>
+							      <td><?php echo $value["last_name"];?></td>
 							      <td><?php echo $value["position"];?></td>
 							      <td><?php echo $value["office"];?></td>
-							      <td><?php echo $value["extn"];?></td>
-								  <td><?php echo $value["start date"];?></td>
+								  <td><?php echo $value["start_date"];?></td>
 								  <td><?php echo $value["salary"];?></td>
-							      <td>
+								  <td>
 							      	<a 	href="#" 
 									  	data-toggle="modal" 
-										data-target="#mdl_empresa" 
+										data-target="#mdlEmpresa" 
 										class="btn btn-primary" 
-									>
+										data-id="<?php echo $value['id'];?>">
 									  <i class="fas fa-pencil-alt"></i>
 							      	</a>
 							      	<button class="btn btn-danger">
